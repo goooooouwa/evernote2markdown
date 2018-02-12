2eb4bc23fff7b17cd9ecd81e6e28850c179a8792
Shunfa Xu <gouwa5700@gmail.com>
2018-02-12 18:08:43 +0800

    insert_front_matters spec

---
+++ spec/insert_front_matters_spec.rb
@@ -0,0 +1,14 @@
+require './src/insert_front_matters.rb'
+
+RSpec.describe 'insert_front_matters' do
+  it "#parse_front_matter" do
+  end
+
+  it "#insert_front_matters" do
+    
+  end
+
+  it "#insert_front_matters" do
+
+  end
+end
