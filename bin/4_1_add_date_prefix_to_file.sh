0e2e9b44f4aaf4edd20c5017cbd8e9b1a7385591
Shunfa Xu <sfxu@thoughtworks.com>
2019-03-21 17:49:42 +0800

    only format files not starting with 0-9

--- bin/4_1_add_date_prefix_to_file.sh
+++ bin/4_1_add_date_prefix_to_file.sh
@@ -3,13 +3,13 @@
 # parse date prefix from front matter and add it to filename
 # date prefix in front matter -> date_prefix-filename
 
-for filename in $PATH_TO_MD/*; do
-  title_line=$(sed '3q;d' $filename)
+for filename in $PATH_TO_MD/[^0-9]*; do
+  title_line=$(sed '3q;d' "$filename")
   title=$(echo $title_line | cut -c 9- | rev | cut -c 2- | rev)   # cut front and back
 
   date_line=$(sed '4q;d' $filename)
   date_prefix=$(echo $date_line | cut -c 7-)
   
-  echo "cp $filename $date_prefix$title.md"
-  cp $filename "$date_prefix$title.md"
+  echo "cp $filename $PATH_TO_MD/$date_prefix-$title.md"
+  cp "$filename" "$PATH_TO_MD/$date_prefix$title.md"
 done
