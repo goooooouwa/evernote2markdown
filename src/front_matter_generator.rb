f9868a4f8f609787c208ca6d45350d326edcbc5c
Shunfa Xu <sfxu@thoughtworks.com>
2020-08-18 17:51:37 +0800

    fix lint issues

--- src/front_matter_generator.rb
+++ src/front_matter_generator.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'ruby-pinyin'
 
 def run
@@ -26,11 +29,11 @@ def insert_title(filename)
   puts "insert front matter 'title: #{basename}'? [Y/n]"
   insert_title = 'y'
   # insert_title = gets.chomp
-  unless insert_title.downcase == 'n'
-    puts "gsed -i '1 a title: #{basename}' \"#{filename}\""
-    `gsed -i '1 a title: #{basename}' "#{filename}"`
-    puts 'inserted'
-  end
+  return if insert_title.downcase == 'n'
+
+  puts "gsed -i '1 a title: #{basename}' \"#{filename}\""
+  `gsed -i '1 a title: #{basename}' "#{filename}"`
+  puts 'inserted'
 end
 
 def insert_created_date(filename)
@@ -43,22 +46,22 @@ def insert_created_date(filename)
   puts "insert front matter 'date: #{created_at}'? [Y/n]"
   insert_date = 'y'
   # insert_date = gets.chomp
-  unless insert_date.downcase == 'n'
-    puts "gsed -i '1 a date: #{created_at}' \"#{filename}\""
-    `gsed -i '1 a date: #{created_at}' "#{filename}"`
-    puts 'inserted'
-  end
+  return if insert_date.downcase == 'n'
+
+  puts "gsed -i '1 a date: #{created_at}' \"#{filename}\""
+  `gsed -i '1 a date: #{created_at}' "#{filename}"`
+  puts 'inserted'
 end
 
 def insert_category(filename)
   category = File.dirname(filename).split('/').last
   puts "insert front matter 'category: #{category}'? [Y/n]"
   insert_category = gets.chomp
-  unless insert_category.downcase == 'n'
-    puts "gsed -i '1 a category: #{category}' \"#{filename}\""
-    `gsed -i '1 a category: #{category}' "#{filename}"`
-    puts 'inserted'
-  end
+  return if insert_category.downcase == 'n'
+
+  puts "gsed -i '1 a category: #{category}' \"#{filename}\""
+  `gsed -i '1 a category: #{category}' "#{filename}"`
+  puts 'inserted'
 end
 
 def convert_filename_to_pinyin(filename)
@@ -74,11 +77,11 @@ def convert_filename_to_pinyin(filename)
   puts "convert filename from '#{filename}' to '#{converted_filename}'? [Y/n]"
   convert_filename = 'y'
   # convert_filename = gets.chomp
-  unless convert_filename.downcase == 'n'
-    puts "mv \"#{filename}\" \"#{dirname}/#{converted_filename}\""
-    `mv "#{filename}" "#{dirname}/#{converted_filename}"`
-    puts 'converted'
-  end
+  return if convert_filename.downcase == 'n'
+
+  puts "mv \"#{filename}\" \"#{dirname}/#{converted_filename}\""
+  `mv "#{filename}" "#{dirname}/#{converted_filename}"`
+  puts 'converted'
 end
 
 def add_current_date_prefix_to_filename(filename)
@@ -88,11 +91,11 @@ def add_current_date_prefix_to_filename(filename)
   filename_with_date_prefix = "#{current_date}-#{basename}.md"
   puts "add date prefix '#{current_date}-'? [Y/n]"
   add_date_prefix = gets.chomp
-  unless add_date_prefix.downcase == 'n'
-    puts "mv \"#{filename}\" \"#{dirname}/#{filename_with_date_prefix}\""
-    `mv "#{filename}" "#{dirname}/#{filename_with_date_prefix}"`
-    puts 'prefix added'
-  end
+  return if add_date_prefix.downcase == 'n'
+
+  puts "mv \"#{filename}\" \"#{dirname}/#{filename_with_date_prefix}\""
+  `mv "#{filename}" "#{dirname}/#{filename_with_date_prefix}"`
+  puts 'prefix added'
 end
 
 def add_created_date_prefix_to_filename(filename)
@@ -102,11 +105,11 @@ def add_created_date_prefix_to_filename(filename)
   filename_with_date_prefix = "#{created_at}-#{basename}.md"
   puts "add date prefix '#{current_date}-'? [Y/n]"
   add_date_prefix = gets.chomp
-  unless add_date_prefix.downcase == 'n'
-    puts "mv \"#{filename}\" \"#{dirname}/#{filename_with_date_prefix}\""
-    `mv "#{filename}" "#{dirname}/#{filename_with_date_prefix}"`
-    puts 'prefix added'
-  end
+  return if add_date_prefix.downcase == 'n'
+
+  puts "mv \"#{filename}\" \"#{dirname}/#{filename_with_date_prefix}\""
+  `mv "#{filename}" "#{dirname}/#{filename_with_date_prefix}"`
+  puts 'prefix added'
 end
 
 # date.md format:
