13ce22bcc279143476919083865c3e206d11711b
Shunfa Xu <sfxu@thoughtworks.com>
2020-10-10 15:05:14 +0800

    use_first_line_as_title

--- src/operations/insert_front_matter.rb
+++ src/operations/insert_front_matter.rb
@@ -7,8 +7,16 @@ def insert_tag(filename)
   ask_question filename, "Insert 'tag: ---'? [Y/n]"
   # return if STDIN.gets.chomp.downcase == 'n'
 
-  execute_command("gsed -i '1 i ---' \"#{filename}\"")
-  execute_command("gsed -i '1 i ---' \"#{filename}\"")
+  puts `gsed -i '1 i ---' "#{filename}"`
+  puts `gsed -i '1 i ---' "#{filename}"`
+end
+
+def use_first_line_as_title(filename)
+  first_line = `head -1 "#{filename}"`.sub(/\n/, '')[0..19]
+  puts `gsed -i '1 i ---' "#{filename}"`
+  puts `gsed -i '1 i ---' "#{filename}"`
+  puts `gsed -i '1 a published: false' "#{filename}"`
+  puts `gsed -i '1 a title: "#{first_line}"' "#{filename}"`
 end
 
 def insert_title(filename)
